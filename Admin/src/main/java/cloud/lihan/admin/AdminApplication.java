be6c3c89212236e6433b7a45708da28fab3c82c7
李汉云 <739125492@qq.com>
2022-07-06 16:53:55 +0800

    添加打包子工程

---
+++ Admin/src/main/java/cloud/lihan/admin/AdminApplication.java
@@ -0,0 +1,13 @@
+package cloud.lihan.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
---
+++ Admin/src/test/java/cloud/lihan/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.lihan.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
